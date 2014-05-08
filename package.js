1310aab9984776a93c9f9c839f4676a5b8418491
Louis DeScioli <louis.descioli@gmail.com>
2014-05-08 01:57:16 -0400

    Meteor fix for slick to not remove templates on slickAdd, and added tests file

--- package.js
+++ package.js
@@ -3,18 +3,21 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-    api.use('jquery', 'client');
+    api.use(['jquery']);
     api.add_files([
         'lib/slick/slick/slick.js',
-        'lib/slick/slick/slick.css'
-        ], 'client'
-    };
-    api.add_files([
+        'lib/slick/slick/slick.css',
         'lib/slick/slick/fonts/slick.eot',
         'lib/slick/slick/fonts/slick.svg',
         'lib/slick/slick/fonts/slick.ttf',
-        'lib/slick/slick/fonts/slick.woff'
-        ], 'server',
-        {isAsset: true}
-    );
+        'lib/slick/slick/fonts/slick.woff',
+        'lib/slick/slick/ajax-loader.gif'
+        ], 'client');
+});
+
+Package.on_test(function(api) {
+    api.use('jquery', 'client');
+    api.use(['tinytest', 'test-helpers'], 'client');
+
+    api.add_files('slick_tests.js', 'client');
 });
